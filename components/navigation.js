308ef4046134b6901040c259ad6f9f12b8e04982
Johin123 <johinkgigi@gmail.com>
2024-09-21 11:32:58 +0530

    navigation change

--- components/navigation.js
+++ components/navigation.js
@@ -2,68 +2,61 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-import React, { useEffect, useState } from 'react'
-import { RxHome } from "react-icons/rx";
-import { FaBuildingUser, FaLaptopCode } from "react-icons/fa6";
-import { GrProjects } from "react-icons/gr";
-import Transition from './transition';
+import React, { useEffect, useState } from 'react';
 
 const Navigation = () => {
-    const [isRouting, setIsRouting] = useState(false)
-    const path = usePathname()
-    const [isActive, setIsActive] = useState(path)
-    const [prevPath, setPrevPath] = useState("/")
+    const [isRouting, setIsRouting] = useState(false);
+    const path = usePathname();
+    const [prevPath, setPrevPath] = useState("/");
 
     useEffect(() => {
         if (prevPath !== path) {
-            setIsRouting(true)
+            setIsRouting(true);
         }
-    }, [path, prevPath])
+    }, [path, prevPath]);
 
     useEffect(() => {
         if (isRouting) {
-            setPrevPath(path)
+            setPrevPath(path);
             const timeout = setTimeout(() => {
-                setIsRouting(false)
-            }, 1200)
-            return () => clearTimeout(timeout)
+                setIsRouting(false);
+            }, 1200);
+            return () => clearTimeout(timeout);
         }
-    }, [isRouting])
+    }, [isRouting]);
 
     const Navlinks = [
         {
-            name: "/",
-            icon: RxHome,
+            name: "Home",
             link: "/",
         },
         {
-            name: "/my-skills",
-            icon: FaLaptopCode,
+            name: "Skills",
             link: "/my-skills",
         },
         {
-            name: "/my-projects",
-            icon: GrProjects,
+            name: "Projects",
             link: "/my-projects",
         },
         {
-            name: "/my-experience",
-            icon: FaBuildingUser,
+            name: "Experience",
             link: "/my-experience",
         },
-    ]
+    ];
 
     return (
         <div className='fixed bottom-5 left-1/2 transform -translate-x-1/2 z-[50] w-[90%] md:w-[60%] lg:w-[40%] max-h-[80px] rounded-full flex justify-around items-center border border-white px-4 py-3 bg-black bg-opacity-80'>
             {/* Transition effect when routing */}
-            {isRouting }
+            {isRouting && <div className="transition-effect">Routing...</div>}
             {Navlinks.map((nav) => (
                 <Link className="min-w-[20%] text-center" key={nav.name} href={nav.link}>
-                    <nav.icon className={`w-[24px] h-[24px] mx-auto ${path === nav.name ? "text-purple-600" : "text-white"}`} />
+                    <span className={`text-lg ${path === nav.link ? "text-purple-600" : "text-white"}`}>
+                        {nav.name}
+                    </span>
                 </Link>
             ))}
         </div>
-    )
+    );
 }
 
-export default Navigation
+export default Navigation;
