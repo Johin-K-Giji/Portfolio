8207fd47592ff8084d46abdb57cf3c7e40e486d1
Johin K Giji <91401048+Johin-K-Giji@users.noreply.github.com>
2024-10-15 16:40:20 +0530

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -4,7 +4,7 @@ import { FaGithub } from "react-icons/fa";
 
 const Navbar = () => {
   return (
-    <div className='fixed top-0 z-[50] w-full h-[100px] bg-transparent flex justify-between items-center px-10 md:px-20'>
+    <div className=' top-0 z-[50] w-full h-[100px] bg-transparent flex justify-between items-center px-10 md:px-20'>
         <div className='flex flex-row gap-6 items-center'>
         <h1 className='text-white text-[25px] font-semibold'>
                 Johin K Gigi
